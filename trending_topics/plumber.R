6b261c3afbf7c66356b94c023afe6e64a25d22e7
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2024-08-01 17:28:34 -0400

    bearer stuff not working, remove for now

--- trending_topics/plumber.R
+++ trending_topics/plumber.R
@@ -4,35 +4,12 @@ source("source_funcs_and_secrets.R")
 #* @apiTitle Trending Topics Pipeline AI
 #* @apiDescription Handles updates & interactions between the trending topics tables including APIs for pulling tweets and automated summaries.
 
-
 #* Echo back the input
 #* @param msg The message to echo
 #* @get /echo
 function(msg = "") {
     list(msg = paste0("The message is: '", msg, "'"))
 }
-
-#* @filter bearer_token_auth
-function(req, res) {
-  auth_header <- req$HTTP_AUTHORIZATION
-
-  if (is.null(auth_header) || !grepl("^Bearer\\s", auth_header)) {
-    res$status <- 401
-    return(list(error = "Unauthorized: Bearer token required"))
-  }
-
-  token <- sub("^Bearer\\s", "", auth_header)
-
-  # Replace 'YOUR_EXPECTED_TOKEN' with your actual bearer token
-  if (token != expected_token) {
-    res$status <- 403
-    return(list(error = "Forbidden: Invalid token"))
-  }
-
-  # If the token is valid, continue with the request
-  plumber::forward()
-}
-
 
 #* Return the sum of two numbers
 #* @param a The first number to add
