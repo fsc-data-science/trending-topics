fbb032ba6575b236786c0f2c963cd8bcc4c78012
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2024-08-01 17:12:50 -0400

    testing bearer calls

--- trending_topics/plumber.R
+++ trending_topics/plumber.R
@@ -11,27 +11,27 @@ source("source_funcs_and_secrets.R")
 function(msg = "") {
     list(msg = paste0("The message is: '", msg, "'"))
 }
-# 
-# #* @filter bearer_token_auth
-# function(req, res) {
-#   auth_header <- req$HTTP_AUTHORIZATION
-#   
-#   if (is.null(auth_header) || !grepl("^Bearer\\s", auth_header)) {
-#     res$status <- 401
-#     return(list(error = "Unauthorized: Bearer token required"))
-#   }
-#   
-#   token <- sub("^Bearer\\s", "", auth_header)
-#   
-#   # Replace 'YOUR_EXPECTED_TOKEN' with your actual bearer token
-#   if (token != expected_token) {
-#     res$status <- 403
-#     return(list(error = "Forbidden: Invalid token"))
-#   }
-#   
-#   # If the token is valid, continue with the request
-#   plumber::forward()
-# }
+
+#* @filter bearer_token_auth
+function(req, res) {
+  auth_header <- req$HTTP_AUTHORIZATION
+
+  if (is.null(auth_header) || !grepl("^Bearer\\s", auth_header)) {
+    res$status <- 401
+    return(list(error = "Unauthorized: Bearer token required"))
+  }
+
+  token <- sub("^Bearer\\s", "", auth_header)
+
+  # Replace 'YOUR_EXPECTED_TOKEN' with your actual bearer token
+  if (token != expected_token) {
+    res$status <- 403
+    return(list(error = "Forbidden: Invalid token"))
+  }
+
+  # If the token is valid, continue with the request
+  plumber::forward()
+}
 
 
 #* Return the sum of two numbers
