cecad8a2450d4be8cc976348a05ff56f5ea20d4e
Carlos R. Mercado <carlos.mercado@flipsidecrypto.com>
2024-08-07 14:15:48 -0400

    Revert "maintainance"
    This reverts commit 5d94216f79b5be425409acbc26d1370498df61e7.

---
+++ trending_ui/www/rudderstack.js
@@ -0,0 +1,3 @@
+!function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],e.factory=function(t){return function(){var r=Array.prototype.slice.call(arguments);return r.unshift(t),e.push(r),e}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://rsp-production.flipsidecrypto.workers.dev/dataPlane";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
+e.load('29a6Pl6fLlx4iYiOdfxRt0IBad9','https://rsp-production.flipsidecrypto.workers.dev'),
+e.page()}();
